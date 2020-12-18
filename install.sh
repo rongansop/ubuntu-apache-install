e9ebadb4691f68d3ce797208d95f37bdd645224d
amidevous <amidevous@users.noreply.github.com>
2020-12-18 22:48:01 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ wget -O- "https://download.opensuse.org/repositories/home:/andykimpe:/ubuntu-$(l
 sudo rm -f /etc/apt/sources.list.d/*andykimpe*.list
 echo 'deb http://download.opensuse.org/repositories/home:/andykimpe:/ubuntu-'$(lsb_release -sc)'/xUbuntu_'$(lsb_release -sr)'/ /' | sudo tee /etc/apt/sources.list.d/andykimpe.list
 apt-get update
-apt-get -y install libapache2-mod-php5.6 php5.6-common php5.6-cli php5.6-mysql php5.6-gd php5.6-mcrypt php5.6-curl php-pear php5.6-imap php5.6-xmlrpc php5.6-xsl php5.6-intl php php-dev php5.6-dev php5.6-mbstring
+apt-get -y install apache2 apache2-bin apache2-data apache2-utils libapache2-mod-php5.6 php5.6 php5.6-common php5.6-cli php5.6-mysql php5.6-gd php5.6-mcrypt php5.6-curl php-pear php5.6-imap php5.6-xmlrpc php5.6-xsl php5.6-intl php php-dev php5.6-dev php5.6-mbstring
 update-alternatives --set php /usr/bin/php5.6
 update-alternatives --set phar /usr/bin/phar5.6
 update-alternatives --set phar.phar /usr/bin/phar.phar5.6
