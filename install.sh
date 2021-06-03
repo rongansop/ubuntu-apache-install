4a1f1e63116d7f279400c550203585b8d8ab749d
amidevous <amidevous@users.noreply.github.com>
2021-06-03 22:56:09 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,27 +1,44 @@
 #!/bin/bash
+VERSION=5.6
+read -e -p "Enter php mumber version 5.6 or 7.0 or 7.1 or 7.3 or 7.4 or 8.0: " -i "$VERSION" VERSION
 apt-get update
-#apt-get -y install python-software-properties
+apt-get -y install python-software-properties
 apt-get -y install software-properties-common wget gnupg gnupg2
 apt-get -y purge php php*
-wget -O- "https://download.opensuse.org/repositories/home:/andykimpe:/ubuntu-$(lsb_release -sc)/xUbuntu_$(lsb_release -sr)/Release.key" | sudo apt-key add -
-echo 'deb http://download.opensuse.org/repositories/home:/andykimpe:/ubuntu-'$(lsb_release -sc)'/xUbuntu_'$(lsb_release -sr)'/ /' > /etc/apt/sources.list.d/andykimpe.list
-echo 'deb-src http://download.opensuse.org/repositories/home:/andykimpe:/ubuntu-'$(lsb_release -sc)'/xUbuntu_'$(lsb_release -sr)'/ /' >> /etc/apt/sources.list.d/andykimpe.list
+apt-get update && apt-get -y dist-upgrade && apt-get -y install curl
+echo 'deb http://download.opensuse.org/repositories/home:/andykimpe/xUbuntu_18.04/ /' | sudo tee /etc/apt/sources.list.d/home:andykimpe.list
+curl -fsSL https://download.opensuse.org/repositories/home:andykimpe/xUbuntu_18.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_andykimpe.gpg > /dev/null
+apt-get update ; apt-get -y dist-upgrade
+apt-get -y install curl libcurl3 libcurl4
+rm -f /etc/apt/sources.list.d/home:andykimpe.list
+add-apt-repository ppa:ondrej/apache2 -y -s
+add-apt-repository ppa:ondrej/php -y -s
 apt-get update
 apt-get -y dist-upgrade
-apt-get -y install apache2 apache2-bin apache2-data apache2-utils libapache2-mod-php5.6 php5.6 php5.6-common php5.6-cli php5.6-mysql php5.6-gd php5.6-mcrypt php5.6-curl php-pear php5.6-imap php5.6-xmlrpc php5.6-xsl php5.6-intl php php-dev php5.6-dev php5.6-mbstring
-update-alternatives --set php /usr/bin/php5.6
-update-alternatives --set phar /usr/bin/phar5.6
-update-alternatives --set phar.phar /usr/bin/phar.phar5.6
-update-alternatives --set phpize /usr/bin/phpize5.6
-update-alternatives --set php-config /usr/bin/php-config5.6
+apt-get -y install apache2 apache2-bin apache2-data apache2-utils php php-dev php-pear
+apt-get -y install libapache2-mod-php5.6 php5.6 php5.6-common php5.6-cli php5.6-mysql php5.6-gd php5.6-mcrypt php5.6-curl php5.6-imap php5.6-xmlrpc php5.6-xsl php5.6-intl php5.6-dev php5.6-mbstring
+apt-get -y install libapache2-mod-php7.0 php7.0 php7.0-common php7.0-cli php7.0-mysql php7.0-gd php7.0-mcrypt php7.0-curl php7.0-imap php7.0-xmlrpc php7.0-xsl php7.0-intl php7.0-dev php7.0-mbstring
+apt-get -y install libapache2-mod-php7.1 php7.1 php7.1-common php7.1-cli php7.1-mysql php7.1-gd php7.1-mcrypt php7.1-curl php7.1-imap php7.1-xmlrpc php7.1-xsl php7.1-intl php5.6-dev php5.6-mbstring
+apt-get -y install libapache2-mod-php7.1 php7.1 php7.1-common php7.1-cli php7.1-mysql php7.1-gd php7.1-mcrypt php7.1-curl php7.1-imap php7.1-xmlrpc php7.1-xsl php7.1-intl php7.1-dev php7.1-mbstring
+apt-get -y install libapache2-mod-php7.2 php7.2 php7.2-common php7.2-cli php7.2-mysql php7.2-gd php7.2-mcrypt php7.2-curl php7.2-imap php7.2-xmlrpc php7.2-xsl php7.2-intl php7.2-dev php7.2-mbstring
+apt-get -y install libapache2-mod-php7.3 php7.3 php7.3-common php7.3-cli php7.3-mysql php7.3-gd php7.3-mcrypt php7.3-curl php7.3-imap php7.3-xmlrpc php7.3-xsl php7.3-intl php7.3-dev php7.3-mbstring
+apt-get -y install libapache2-mod-php7.4 php7.4 php7.4-common php7.4-cli php7.4-mysql php7.4-gd php7.4-mcrypt php7.4-curl php7.4-imap php7.4-xmlrpc php7.4-xsl php7.4-intl php7.4-dev php7.4-mbstring
+apt-get -y install libapache2-mod-php8.0 php8.0 php8.0-common php8.0-cli php8.0-mysql php8.0-gd php8.0-mcrypt php8.0-curl php8.0-imap php8.0-xmlrpc php8.0-xsl php8.0-intl php8.0-dev php8.0-mbstring
+update-alternatives --set php /usr/bin/php$VERSION
+update-alternatives --set phar /usr/bin/phar$VERSION
+update-alternatives --set phar.phar /usr/bin/phar.phar$VERSION
+update-alternatives --set phpize /usr/bin/phpize$VERSION
+update-alternatives --set php-config /usr/bin/php-config$VERSION
 a2dismod php7.0
 a2dismod php7.1
 a2dismod php7.2
 a2dismod php7.3
 a2dismod php7.4
-a2enmod php5.6
-phpenmod -v 5.6 mcrypt
-phpenmod -v 5.6 mbstring
+a2dismod php8.0
+a2dismod php5.6
+a2enmod php$VERSION
+phpenmod -v $VERSION mcrypt
+phpenmod -v $VERSION mbstring
 a2enmod rewrite
 wget https://raw.githubusercontent.com/andykimpe/ubuntu-apache-install/master/apache2.conf -O /etc/apache2/apache2.conf
 FILE1="/home/xtreamcodes/iptv_xtream_codes/wwwdir/index.php"
