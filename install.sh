d2b318b72f118038e06f3197c48f2583e1e0daa6
amidevous <amidevous@users.noreply.github.com>
2022-12-20 22:04:03 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 VERSION=5.6
 PORT=80
 PORTSSL=443
-read -e -p "Enter php mumber version 5.6 or 7.0 or 7.1 or 7.3 or 7.4 or 8.0 or 8.1: " -i "$VERSION" VERSION
+read -e -p "Enter php mumber version 5.6 or 7.0 or 7.1 or 7.3 or 7.4 or 8.0 or 8.1 or 8.2: " -i "$VERSION" VERSION
 read -e -p "Enter apache http port: " -i "$PORT" PORT
 read -e -p "Enter apache https port: " -i "$PORTSSL" PORTSSL
 apt-get update
@@ -24,6 +24,8 @@ apt-get -y install libapache2-mod-php7.3 php7.3 php7.3-common php7.3-fpm php7.3-
 apt-get -y install libapache2-mod-php7.4 php7.4 php7.4-common php7.4-fpm php7.4-cli php7.4-mysql php7.4-gd php7.4-mcrypt php7.4-curl php7.4-imap php7.4-xmlrpc php7.4-xsl php7.4-intl php7.4-dev php7.4-mbstring
 apt-get -y install libapache2-mod-php8.0 php8.0 php8.0-common php8.0-fpm php8.0-cli php8.0-mysql php8.0-gd php8.0-mcrypt php8.0-curl php8.0-imap php8.0-xmlrpc php8.0-xsl php8.0-intl php8.0-dev php8.0-mbstring
 apt-get -y install libapache2-mod-php8.1 php8.1 php8.1-common php8.1-fpm php8.1-cli php8.1-mysql php8.1-gd php8.1-mcrypt php8.1-curl php8.1-imap php8.1-xmlrpc php8.1-xsl php8.1-intl php8.1-dev php8.1-mbstring
+apt-get -y install libapache2-mod-php8.2 php8.2 php8.2-common php8.2-fpm php8.2-cli php8.2-mysql php8.2-gd php8.2-mcrypt php8.2-curl php8.2-imap php8.2-xmlrpc php8.2-xsl php8.2-intl php8.2-dev php8.2-mbstring
+
 update-alternatives --set php /usr/bin/php$VERSION
 update-alternatives --set phar /usr/bin/phar$VERSION
 update-alternatives --set phar.phar /usr/bin/phar.phar$VERSION
@@ -31,14 +33,15 @@ update-alternatives --set phpize /usr/bin/phpize$VERSION
 update-alternatives --set php-config /usr/bin/php-config$VERSION
 update-alternatives --remove-all php-fpm
 rm -f /usr/sbin/php-fpm
-update-alternatives --install /usr/sbin/php-fpm php-fpm /usr/sbin/php-fpm5.6 80
-update-alternatives --install /usr/sbin/php-fpm php-fpm /usr/sbin/php-fpm7.0 70
-update-alternatives --install /usr/sbin/php-fpm php-fpm /usr/sbin/php-fpm7.1 60
-update-alternatives --install /usr/sbin/php-fpm php-fpm /usr/sbin/php-fpm7.2 50
-update-alternatives --install /usr/sbin/php-fpm php-fpm /usr/sbin/php-fpm7.3 40
-update-alternatives --install /usr/sbin/php-fpm php-fpm /usr/sbin/php-fpm7.4 30
-update-alternatives --install /usr/sbin/php-fpm php-fpm /usr/sbin/php-fpm8.0 20
-update-alternatives --install /usr/sbin/php-fpm php-fpm /usr/sbin/php-fpm8.1 10
+update-alternatives --install /usr/sbin/php-fpm php-fpm /usr/sbin/php-fpm5.6 90
+update-alternatives --install /usr/sbin/php-fpm php-fpm /usr/sbin/php-fpm7.0 80
+update-alternatives --install /usr/sbin/php-fpm php-fpm /usr/sbin/php-fpm7.1 70
+update-alternatives --install /usr/sbin/php-fpm php-fpm /usr/sbin/php-fpm7.2 60
+update-alternatives --install /usr/sbin/php-fpm php-fpm /usr/sbin/php-fpm7.3 50
+update-alternatives --install /usr/sbin/php-fpm php-fpm /usr/sbin/php-fpm7.4 40
+update-alternatives --install /usr/sbin/php-fpm php-fpm /usr/sbin/php-fpm8.0 30
+update-alternatives --install /usr/sbin/php-fpm php-fpm /usr/sbin/php-fpm8.1 20
+update-alternatives --install /usr/sbin/php-fpm php-fpm /usr/sbin/php-fpm8.2 10
 update-alternatives --set php-fpm /usr/sbin/php-fpm$VERSION
 a2dismod php5.6
 a2dismod php7.0
@@ -48,6 +51,7 @@ a2dismod php7.3
 a2dismod php7.4
 a2dismod php8.0
 a2dismod php8.1
+a2dismod php8.2
 a2enmod php$VERSION
 phpenmod -v 5.6 mcrypt
 phpenmod -v 5.6 mbstring
@@ -65,6 +69,8 @@ phpenmod -v 8.0 mcrypt
 phpenmod -v 8.0 mbstring
 phpenmod -v 8.1 mcrypt
 phpenmod -v 8.1 mbstring
+phpenmod -v 8.2 mcrypt
+phpenmod -v 8.2 mbstring
 a2enmod rewrite
 #a2dismod mpm_prefork
 #a2enmod mpm_event
@@ -78,6 +84,7 @@ a2disconf php7.3-fpm
 a2disconf php7.4-fpm
 a2disconf php8.0-fpm
 a2disconf php8.1-fpm
+a2disconf php8.2-fpm
 #a2enconf php$VERSION-fpm
 systemctl stop php5.6-fpm
 systemctl disable php5.6-fpm
@@ -95,13 +102,15 @@ systemctl stop php8.0-fpm
 systemctl disable php8.0-fpm
 systemctl stop php8.1-fpm
 systemctl disable php8.1-fpm
+systemctl stop php8.2-fpm
+systemctl disable php8.2-fpm
 #systemctl start php$VERSION-fpm
 #systemctl enable php$VERSION-fpm
 wget https://raw.githubusercontent.com/andykimpe/ubuntu-apache-install/master/apache2.conf -O /etc/apache2/apache2.conf
 sed -i "s/80/$PORT/g" "/etc/apache2/ports.conf"
-sed -i "s/443/$PORTSSL/g" "/etc/apache2/ports.conf"
-sed -i "s/80/$PORT/g" "/etc/apache2/sites-available/000-default.conf"
-sed -i "s/443/$PORTSSL/g" "/etc/apache2/sites-available/default-ssl.conf"
+sed -i "s/444/$PORTSSL/g" "/etc/apache2/ports.conf"
+sed -i "s/8080/$PORT/g" "/etc/apache2/sites-available/000-default.conf"
+sed -i "s/444/$PORTSSL/g" "/etc/apache2/sites-available/default-ssl.conf"
 wget http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
 tar xzf ioncube_loaders_lin_x86-64.tar.gz -C /usr/local && rm-f ioncube_loaders_lin_x86-64.tar.gz
 wget https://raw.githubusercontent.com/amidevous/ubuntu-apache-install/master/5.6/php.ini -O /etc/php/5.6/apache2/php.ini
